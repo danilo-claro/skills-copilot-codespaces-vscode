c46fb6fcd716a801d04480226b05e4b883909973
danilo-claro <151655949+danilo-claro@users.noreply.github.com>
2023-11-22 11:58:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var skills = ["HTML", "CSS", "JS", "React", "Node"];
+  var member = {
+    name: "John",
+    age: 25,
+    skills: skills,
+  };
+  // Only change code below this line.
+  return member;
+  // Only change code above this line.
+}
